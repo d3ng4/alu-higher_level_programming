7f9fb7d355cf0929556a03056ceb0fb6a561dc2e
joak <j.deng@alustudent.com>
2022-06-28 02:04:21 -0700

    create sentence

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpretaed, interactive, object-oriented programming/
        Language that combine remarkable power with very clear syntax"
-str = str[39.66] + str[106:112] + str[6]
+str = str[39.66] + str[-22:-17] + str[:6]
 print(str)
