639c85431e1a0055cf225be2f144b54499df0f10
joak <j.deng@alustudent.com>
2022-06-28 02:07:08 -0700

    create sentence

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpretaed, interactive, object-oriented programming/
+str = "Python is an interpretaed, interactive, object-oriented programming\
        Language that combine remarkable power with very clear syntax"
 str = str[39.67] + str[-22:-17] + str[:6]
 print(str)
