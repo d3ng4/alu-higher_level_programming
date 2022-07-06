93078d812d7e897b195b8dab176add37f6d10ad7
joak <j.deng@alustudent.com>
2022-07-06 05:19:48 -0700

    print

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(ord("a"), ord("z") + 1):
     d3ng = chr(i)
-    if d3ng not "q" and d3ng is not "e"
+    if d3ng not "qe":
     print("{:s}".format(d3ng), end="")
