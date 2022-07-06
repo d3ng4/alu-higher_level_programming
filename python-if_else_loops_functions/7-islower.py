94b42ad808fbbf35cef75818e009502b18534f6d
joak <j.deng@alustudent.com>
2022-07-06 11:18:16 -0700

    uppercase

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -3,7 +3,7 @@ def islower(c):
     value = ord(c)
     if value >= 97 and value <= 122:
         return True
-    ielif value >= 48 and value <=57:
+    elif value >= 48 and value <=57:
         return False
     elif value >= 65 and value <= 90:
         return False
