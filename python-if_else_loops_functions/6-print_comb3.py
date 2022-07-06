55489b07675cb660847c60dd930bd82b49a720df
joak <j.deng@alustudent.com>
2022-07-06 10:53:57 -0700

    possible combinations

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for a in range(0, 90):
-    if a !=89:
+    if a != 89:
         if a % 10 > a / 10:
-            print("{02d}".format(i), end=",")
+            print("{02d}".format(i), end=", ")
         else:
             print("{:d}".format(a))
        
