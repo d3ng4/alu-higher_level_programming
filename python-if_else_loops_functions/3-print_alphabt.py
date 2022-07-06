17745b5d2208bc84ee432e2fe0d108298a563c79
joak <j.deng@alustudent.com>
2022-07-06 05:32:37 -0700

    print

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z') +1):
-    if chr(a) is not "q" and chr(a) is not "e":
-        print("{:s}".format(chr(a)), end="")
+    joak = chr(i)
+    if joak not in "qe":
+        print("{:s}".format(joak), end="")
