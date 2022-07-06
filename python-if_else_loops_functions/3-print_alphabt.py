0c18b61b40f0c39d5467f317db0712a5719f77a4
joak <j.deng@alustudent.com>
2022-07-06 06:21:58 -0700

    commit

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') +1):
+ for i in range(ord('a'), ord('z') +1):
     joak = chr(i)
     if joak not in "qe":
         print("{:s}".format(joak), end="")
