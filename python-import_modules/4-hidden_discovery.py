44f37a9b63ffe4175c2ab2c7da68fa90ab03d7ed
joak <j.deng@alustudent.com>
2022-07-07 17:58:30 -0700

    defines

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
-#!/usr/bin/python3.4
+#!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
-    for s in dir():
-        if s[:2] != "__":
-            print("{}".format(s))
+    import hidden_4 as extra
+
+
+for x in dir(extra):
+    if x[0:2] != "__":
+        print("{:s}".format(x)))
