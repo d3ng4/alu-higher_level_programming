3da6e3f49e4762a44dc149e4ba99db6abf045a9b
joak <j.deng@alustudent.com>
2022-07-08 02:46:07 -0700

    prints names

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-if__name__ == "__main__":
-    import hidden_4 as extra
-    for x in dir(extra):
-        if x[0:2] != "__":
-            print("{:s}".format(x))
+import hidden_4
+
+
+def main():
+    for name in dir(hidden_4):
+        if name.startswith('__'):
+            continue
+        print(name)
+
+
+if __name__ == '__main__':
+    main()
