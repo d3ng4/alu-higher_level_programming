117136c72652f7b0891780354bcc9479bade7564
joak <j.deng@alustudent.com>
2022-07-15 05:32:33 -0700

    weight

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    return sum([x[0] * x[1] for in my_list]) / \
-            (sum([x[1] for x in my_list]) or 1)
+    if my_list:
+        return (sum([a * b for a, b in my_list])/sum(b for a, b in my_list))
+    return 0)
