528fccc444a1b8c48b6d3d2857592bfe778097c9
joak <j.deng@alustudent.com>
2022-07-18 06:53:37 -0700

    integer division

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -5,5 +5,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         a = None
     finally:
-        print("Inside result:", "{}".format(a))
-    return a 
+        print(Inside result:", "{}".format(a))
+    return a    
