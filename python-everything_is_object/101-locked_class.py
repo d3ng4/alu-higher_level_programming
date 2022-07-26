e37de6b5cce9c0f31445c2b5d08a01e58e9d683b
joak <j.deng@alustudent.com>
2022-07-26 09:18:33 -0700

    objects and executions

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+***This module contains a locked class***
+
 class LockedClass:
+    ***This class has no class or objects attributes***
     __slots__ = ['first_name']
-    def __init__(self, first_name=''):
-        self.first_name = first_namee
