bee0864fcc35262ca25a91cb364e0ea0abc3d797
joak <j.deng@alustudent.com>
2022-07-27 01:59:35 -0700

    list of available attributes and methods

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+0-lookup
+function that returns the list of attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """returns list of attributes and methods"""
+    return (dir(obj))
