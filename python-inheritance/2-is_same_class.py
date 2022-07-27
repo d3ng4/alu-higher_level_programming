19f62b3ee11b9cf3bec7cbb7fa2be0a5e30518c6
joak <j.deng@alustudent.com>
2022-07-27 02:32:02 -0700

    exact same object

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module containing 'is same_class' function"""
+
+
+def is_same_class(obj, a_class):
+    """
+    function checking for object of specified class
+    """
+    return (type(obj) == a_class)
