2cb7acea47e46aed0a81d60e059d358f54a1bf29
joak <j.deng@alustudent.com>
2022-07-27 02:49:03 -0700

    sub class

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-module containing the inherits_from function
+Module containing the inherits_from function
 """
 
 
 def inherits_from(obj, a_class):
     """
-    function returns True or False
+    Function returns True or False
     """
     return(isinstance(obj), a_class) and type(obj) is not a_class)
