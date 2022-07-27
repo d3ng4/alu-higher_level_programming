b37a06d1d9929906f1ab9049bff7114fbe95fd44
joak <j.deng@alustudent.com>
2022-07-27 04:07:31 -0700

    basegeometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+module containing basegeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    class base has public
+    """
+    def area(self):
+        """
+        raises an exception
+        """
+        raise Exception("area() is not implemented")
+
