fb9149346861908c1590e13db42d41b0505eca8b
joak <j.deng@alustudent.com>
2022-07-28 14:56:03 -0700

    task6

--- python-network_0/6-peak.py
+++ python-network_0/6-peak.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
-""" Find a peak """
+''' find peak '''
 
 
 def find_peak(list_of_integers):
-    """ Function to find a peak on an unsorted list. """
-    if not list_of_integers:
-        return None
-
-    if len(list_of_integers) <= 3:
-        return max(list_of_integers)
-
-    middle = len(list_of_integers) // 2
-    if list_of_integers[middle-1] <= list_of_integers[middle]\
-       >= list_of_integers[middle+1]:
-        return list_of_integers[middle]
-
-    if list_of_integers[middle-1] > list_of_integers[middle]:
-        return find_peak(list_of_integers[0:middle-1])
-
-    if list_of_integers[middle] < list_of_integers[middle+1]:
-        return find_peak(list_of_integers[middle+1:])
+    ''' find peak '''
+    if list_of_integers:
+        list_of_integers.sort()
+        return (list_of_integers[-1])
+    else:
+        return None)
