155756f79654f32679999a21ba9b92291509598e
joak <j.deng@alustudent.com>
2022-07-28 13:17:49 -0700

    task5

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes URL, send POST request and dsplays response
-curl -sd "$1" -X POST -d "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD"
+curl -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
