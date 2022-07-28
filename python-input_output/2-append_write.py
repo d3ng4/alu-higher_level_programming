107bcc3aa45fc54c014f7e33285649feeda0d6aa
joak <j.deng@alustudent.com>
2022-07-28 03:18:53 -0700

    append file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""Defines a file-appending function."""
+"""
+This file contains function that appends
+a string at the end of a text file and
+returns number of characters added
+"""
 
 
 def append_write(filename="", text=""):
