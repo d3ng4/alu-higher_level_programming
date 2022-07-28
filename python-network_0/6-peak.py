383c5c4d7f54972d9b2c2a6ba7f705faf98da8e9
joak <j.deng@alustudent.com>
2022-07-28 15:01:50 -0700

    task6

--- python-network_0/6-peak.py
+++ python-network_0/6-peak.py
@@ -3,9 +3,11 @@
 
 
 def find_peak(list_of_integers):
-    """find peak"""
+    """
+    find peak
+    """
     if list_of_integers:
         list_of_integers.sort()
         return (list_of_integers[-1])
     else:
-        return None)
+        return None
