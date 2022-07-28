fe7cd90a619d9e5ec9e90556ef658aea6ed1358e
joak <j.deng@alustudent.com>
2022-07-28 14:40:38 -0700

    task4

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in URL and sends GET request to URL and displays it
-curl -sX "$1" "GET" -H "X-HolbertonSchool-User-Id: 98"
+curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
