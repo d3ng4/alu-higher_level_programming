a98594419cb51524f08001d37599723d092e14dd
joak <j.deng@alustudent.com>
2022-08-01 10:55:39 -0700

    task1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -5,5 +5,5 @@ import sys
 
 
 if __name__ == "__main__":
-    with request.urlopen(sys.argv[1]) as res:
+    with urllib.request.urlopen(sys.argv[1]) as res:
         print(res.info()['X-Request-Id'])
