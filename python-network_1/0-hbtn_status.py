277460508bfb711c3eb158300c33850632bdd3b2
joak <j.deng@alustudent.com>
2022-08-01 09:27:30 -0700

    fetches intranet status

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import urllib.request
+"""fetches https://intranet.hbtn.io/statua"""
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status')
+        body = reply.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode(encoding="utf -8")))
