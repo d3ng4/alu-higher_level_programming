77769807dc5d9a97efbad8116404bf11fdf2a1de
joak <j.deng@alustudent.com>
2022-08-01 09:29:31 -0700

    fetches intranet status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import urllib.request
-"""fetches https://intranet.hbtn.io/status"""
-
+"""
+Script that fetches https://intranet.hbtn.io/status
+"""
 
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status')
