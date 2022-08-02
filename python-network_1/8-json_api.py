f686053a4108e424f8f9affbdd575641269feb69
joak <j.deng@alustudent.com>
2022-08-02 08:17:43 -0700

    task8

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -5,13 +5,19 @@ import sys
 
 
 if __name__ == "__main__":
-    q = argv[1] if len(argv) > 1 else ""
+    if len(sys.argv) > 1:
+        arg = sys.argv[1]
+    else:
+        arg = ""
+    payload = {'q': arg}
+    url = "http://0.0.0.0:5000/search_user"
+    r = requests.post(url, data=payload)
     try:
-        re = requests.post('http://0.0.0.0:5000/search_user',
-                           data={'q': q}).json()
-        if 'id' in re and 'name' in re:
-            print("[{}] {}".format(re['id'], re['name']))
-        else:
+        r.raise_for_status()
+        json = r.json()
+        if len(json) == 0:
             print("No result")
-    except ValueError:
-        print("Not a valid JSON"
+        else:
+            print("[{:d}] {}".format(json['id'], json['name']))
+    except Exception:
+        print("Not a valid JSON")
