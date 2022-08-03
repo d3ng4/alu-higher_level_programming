48c7a5c75c017a5f2758010089950a2521976337
joak <j.deng@alustudent.com>
2022-08-03 12:02:53 -0700

    task10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
--- Import the database dump from hbtn_0d_tvshows to your MySQL server:
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
