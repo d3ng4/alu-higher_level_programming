4697fffe79f672a1462f125d145620e16c910a27
joak <j.deng@alustudent.com>
2022-08-04 05:15:16 -0700

    task12

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
--- Import the database dump from hbtn_0d_tvshows
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT OUTER JOIN tv_shows_genres ON tv_show_genres.show_id = tv_show.id
-WHERE tv_show_genres.show_id IS NULL
+-- scripts to list all shoes contained in db without genre
+SELECT tv_shows.title, tv_show_genres.genre_id 
+FROM tv_shows 
+LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
+WHERE tv_show_genres.genre_id IS NULL 
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
