28e334a1ccd69803f235ea4f1347bd2812e34f8d
joak <j.deng@alustudent.com>
2022-08-04 06:17:49 -0700

    advanced

---
+++ python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""script that takes 2 arguments in order to solve this challenge"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    # Get the arguments, arg1: repo name, arg2: repo owner
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+            sys.argv[2], sys.argv[1])
+
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print(f"{commits[i]['sha']}: {commits[i]['commit']['author']['name']}")
+    except IndexError:
+        pass
