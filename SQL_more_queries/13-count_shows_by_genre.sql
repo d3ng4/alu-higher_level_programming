eb98f09df608dc1d6ed9884c5e8cfe6432afadfa
joak <j.deng@alustudent.com>
2022-08-04 05:47:02 -0700

    task 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
--- script that list all genres from tv show and display number of shows linked to each
-SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
+3-- script that list all genres from tv show and display number of shows linked to each
+SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
 FROM tv_show_genres
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
