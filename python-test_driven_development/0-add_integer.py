550006fe2348162f35fe2904d7e317c7b486d2ab
joak <j.deng@alustudent.com>
2022-08-09 09:22:56 -0700

    task1

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,11 +1,15 @@
-#!/usr/bin/python3
-""" adds two integers"""
+#!usr/bin/python3
+"""unction that adds 2 integers"""
 
 
 def add_integer(a, b=98):
-    """ add number """
-    if type(a) != int and type(a) != float:
-        raise TypeError('a must be an integer')
-    elif type(b) != int and type(b) != float:
-        raise TypeError('b must be an integer')
-    return int(a) + int(b)
+    """ function """
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    if type(a) is not int:
+        raise TypeError("a must be an integer")
+    if type(b) is not int:
+        raise TypeError("b must be an integer")
+    return a + b
