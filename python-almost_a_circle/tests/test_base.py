9ca818138e492bd547eac158afa1f61e5af37904
joak <j.deng@alustudent.com>
2022-08-11 14:18:17 -0700

    test

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,80 +1,46 @@
 #!/usr/bin/python3
-""" Base unit testing """
+"""
+unit testing
+"""
+
 import unittest
-import pep8
-import inspect
 import json
 from models import base
 Base = base.Base
 
 
-class TestBaseTypos(unittest.TestCase):
-    """ Tests base documentation """
-
-    @classmethod
-    def setUpClass(cls):
-        """ Set up for tests """
-        cls.base_funcs = inspect.getmembers(Base, inspect.isfunction)
-
-    def test_pep8(self):
-        """ Test that models/base.py conforms to PEP8 """
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/base.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-    def test_pep8_base(self):
-        """ Test the test file xD """
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['tests/test_models/test_base.py'])
-
-    def test_module(self):
-        """ Tests module """
-        self.assertTrue(len(base.__doc__) >= 1)
-
-    def test_class(self):
-        """ Tests class """
-        self.assertTrue(len(Base.__doc__) >= 1)
-
-    def test_funcions(self):
-        """ Tests functions """
-        for func in self.base_funcs:
-            self.assertTrue(len(func[1].__doc__) >= 1)
-
-
-class TestBaseFunctions(unittest.TestCase):
-    """ Test base class """
-
-    def test_arguments_overflow(self):
-        """ Send many arguments """
+class TestBase(unittest.TestCase):
+    """testing Base class"""
+    def _too_many_args(self):
+        """testing args"""
         with self.assertRaises(TypeError):
             b = Base(1, 1)
 
-    def test_no_id(self):
-        """ Do not send id """
+    def _no_id(self):
+        """Testing id as None"""
         b = Base()
         self.assertEqual(b.id, 1)
 
-    def test_id_set(self):
-        """ Send id normally """
+    def _id_set(self):
+        """Testing id as not None"""
         b98 = Base(98)
         self.assertEqual(b98.id, 98)
 
-    def test_no_id_after_set(self):
-        """ Change id after setter """
+    def _no_id_after_set(self):
+        """Testing id as None after not None"""
         b2 = Base()
         self.assertEqual(b2.id, 2)
 
-    def test_nb(self):
-        """ Tests objects as a private attribute """
+    def _nb_private(self):
+        """Testing nb_objects as a private instance attribute"""
         b = Base(3)
         with self.assertRaises(AttributeError):
             print(b.nb_objects)
         with self.assertRaises(AttributeError):
             print(b.__nb_objects)
 
-    def test_json_string(self):
-        """ Tests json string function """
+    def _to_json_string(self):
+        """Testing regular to json string"""
         Base._Base__nb_objects = 0
         d1 = {"id": 9, "width": 5, "height": 6, "x": 7, "y": 8}
         d2 = {"id": 2, "width": 2, "height": 3, "x": 4, "y": 0}
@@ -83,22 +49,22 @@ class TestBaseFunctions(unittest.TestCase):
         d = json.loads(json_s)
         self.assertEqual(d, [d1, d2])
 
-    def test_empty_json_string(self):
-        """ Send empty string to json function """
+    def _empty_to_json_string(self):
+        """Test for passing empty list"""
         json_s = Base.to_json_string([])
         self.assertTrue(type(json_s) is str)
         self.assertEqual(json_s, "[]")
 
-    def test_None_to_json_String(self):
-        """ Send none to json function """
+    def _None_to_json_String(self):
+        """testting None to a json"""
         json_s = Base.to_json_string(None)
         self.assertTrue(type(json_s) is str)
         self.assertEqual(json_s, "[]")
 
-    def test_from_json_string(self):
-        """ Regular data to open """
+    def _from_json_string(self):
+        """Tests normal from_json_string"""
         json_str = '[{"id": 9, "width": 5, "height": 6, "x": 7, "y": 8}, \
-                    {"id": 2, "width": 2, "height": 3, "x": 4, "y": 0}]'
+{"id": 2, "width": 2, "height": 3, "x": 4, "y": 0}]'
         json_l = Base.from_json_string(json_str)
         self.assertTrue(type(json_l) is list)
         self.assertEqual(len(json_l), 2)
@@ -109,10 +75,10 @@ class TestBaseFunctions(unittest.TestCase):
         self.assertEqual(json_l[1],
                          {"id": 2, "width": 2, "height": 3, "x": 4, "y": 0})
 
-    def test_fjs_empty(self):
-        """ Send empty string """
+    def _frjs_empty(self):
+        """Tests from_json_string  empty string"""
         self.assertEqual([], Base.from_json_string(""))
 
-    def test_fjs_None(self):
-        """ Send None """
+    def _frjs_None(self):
+        """Testing from_json_string   none string"""
         self.assertEqual([], Base.from_json_string(None))
