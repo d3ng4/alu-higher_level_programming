cc2aea55ef55de591b28fb1321ee2ed5d4f8127b
joak <j.deng@alustudent.com>
2022-08-15 11:03:27 -0700

    citites from database

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-""" lists all cities from the database hbtn_0e_4_usa """
+"""
+Script to list all cities from the db
+takes 3 arguments
+"""
 
 
 if __name__ == "__main__":
@@ -7,7 +10,7 @@ if __name__ == "__main__":
     import MySQLdb
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-     cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
     JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
     lst = cur.fetchall()
     for r in lst:
