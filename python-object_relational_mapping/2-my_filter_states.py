56abaf31f0bd64f34e8c2b2ed916a67678083fb6
joak <j.deng@alustudent.com>
2022-08-15 10:57:00 -0700

    task2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,15 +8,13 @@ of hbtn_0e_0_usa where name matches the argument
 if __name__ == "__main__":
     from sys import argv
     import MySQLdb
-    db = MySQLdb.connect(user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
-    cr = db.cursor()
-    cr.execute("SELECT * from states\
-                WHERE name LIKE '{}' COLLATE latin1_general_cs\
-                ORDER BY states.id".format(argv[4]))
-    states = cr.fetchall()
-    for state in states:
-        print(state)
-    cr.close()
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}'\
+    ORDER BY states.id ASC".format(argv[4]))
+    lst = cur.fetchall()
+    for r in lst:
+        if r[1] == argv[4]:
+            print(r)
+    cur.close()
     db.close()
