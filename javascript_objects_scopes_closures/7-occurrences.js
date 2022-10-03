cbed62600418b1524105351e0d3a22dc6432f9be
d3ng4 <denggi2000@gmail.com>
2022-10-03 05:17:20 -0700

    count

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
 exports.nbOccurrences = function (list, searchElement) {
-  return list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
+  let count = 0;
+	let i = 0;
+	for (i = 0; i < list.length; i++) {
+		if (searchElement == list[i]) {
+			count++;
+		}
+	}
+	return count;
 };
