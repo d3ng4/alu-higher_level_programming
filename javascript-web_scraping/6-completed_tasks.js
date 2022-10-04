f4a357f14aca0a129c4670ce7a6b5e029ae08a43
d3ng4 <denggi2000@gmail.com>
2022-10-04 01:08:02 -0700

    no of tasks completed

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,15 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
-
-request.get(process.argv[2], function (err, response, body) {
-  if (err) throw err;
-  if (response.statusCode === 200) {
-    let done = {};
-    for (let task of JSON.parse(body)) {
-      if (task.completed === true) {
-        if (task.userId in done) { done[task.userId] += 1; } else { done[task.userId] = 1; }
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
-    }
-    console.log(done);
+    });
+    console.log(completed);
   }
 });
