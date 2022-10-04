855985e4303626ae49547778e6e52be51c442b4d
d3ng4 <denggi2000@gmail.com>
2022-10-04 01:26:35 -0700

    starwars characters

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-let request = require('request');
-let movie = 'http://swapi.co/api/films/';
+const request = require('request');
+const movie = 'http://swapi.co/api/films/';
 
 request.get(movie + process.argv[2], function (err, response, body) {
   if (err) throw err;
   if (response.statusCode === 200) {
-    let everything = JSON.parse(body);
-    let listch = [];
-    for (let charac of everything.characters) {
+    const everything = JSON.parse(body);
+    const listch = [];
+    for (const charac of everything.characters) {
       listch.push(new Promise(function (resolve, reject) {
         request.get(charac, function (err, response, body) {
           if (err) throw err; if (response.statusCode === 200) { resolve(JSON.parse(body).name); } else { reject(Error('Unknown')); }
